06199464a5d938607259b9f1929470b239a8fa11
arash-shafiee <i.arash.shafiee@gmail.com>
2024-10-20 18:01:54 +0330

    feat: servcie generator refactor rest api prompts, add icon service prompts

--- generators/service/index.ts
+++ generators/service/index.ts
@@ -1,15 +1,24 @@
 import { PlopGeneratorConfig } from "plop";
 import { directorySelect } from "../core";
 import { readdirSync } from "fs";
+import { config } from "../config";
 
+type TServices = "rest-api" | "icon";
 interface IServicePrompts {
 	name: string;
 	path: string;
+	serviceTypes: Set<TServices>;
+	imagesPath: string;
 }
 const serviceGenerator: PlopGeneratorConfig = {
 	description: "create service",
 	prompts: async (inquirer): Promise<IServicePrompts> => {
-		const result = { path: "", name: "" };
+		const result: IServicePrompts = {
+			path: "",
+			name: "",
+			serviceTypes: new Set<TServices>(),
+			imagesPath: "",
+		};
 
 		const path = await directorySelect({
 			basePath: "./src/pages",
@@ -21,6 +30,7 @@ const serviceGenerator: PlopGeneratorConfig = {
 		});
 		result.path = path;
 		const filename = path.split("/").at(-1);
+		result.imagesPath = path.replace("/src/pages", "");
 
 		if (filename) {
 			result.name = filename;
@@ -33,21 +43,78 @@ const serviceGenerator: PlopGeneratorConfig = {
 			result.name = name.name;
 		}
 
+		const services = await inquirer.prompt({
+			message: "Select types of serivces: ",
+			name: "types",
+			type: "checkbox",
+			choices: [
+				{ name: "Icon", value: "icon" },
+				{ name: "Rest API", value: "rest-api" },
+			],
+			validate: (value) => {
+				if (
+					(Array.isArray(value) && value.length === 0) ||
+					!Array.isArray(value)
+				) {
+					return "Select at least one types of service";
+				}
+				return true;
+			},
+		});
+		result.serviceTypes = new Set<TServices>(...services.types);
+
 		return Promise.resolve(result);
 	},
 	actions: [
+		/*** create servcies folder (start) ***/
+		{
+			type: "add",
+			path: "{{ path }}/services/index.ts",
+			template: "",
+		},
+		/*** create servcies folder (end) ***/
+
+		/*** Rest API section (start) ***/
 		{
 			type: "addMany",
-			// TODO: create templates for service and implement addMany action to copy them
 			base: "generators/service/templates/module",
 			destination: "{{ path }}/services",
 			templateFiles: "generators/service/templates/module/**",
 			stripExtensions: ["hbs"],
 			skip: (data: IServicePrompts) => {
-				console.log(data.name);
-				return true;
+				return !data.serviceTypes.has("rest-api");
 			},
 		},
+		{
+			type: "append",
+			skip: (data: IServicePrompts) => {
+				return !data.serviceTypes.has("rest-api");
+			},
+			template:
+				"export { {{ pascalCase name }}ApiService } from './api.service.ts';",
+		},
+		/*** Rest API section (end) ***/
+
+		/*** icon service (start) ***/
+		{
+			type: "append",
+			skip: (data: IServicePrompts) => {
+				return !data.serviceTypes.has("icon");
+			},
+			template:
+				"export { {{ pascalCase name }}Vecotr } from './vector.service.ts';",
+		},
+		{
+			type: "add",
+			templateFile: "generators/service/templates/vector.svg",
+			path: `${config.assetsPath}/{{ imagesPath }}/{{ dashCase name }}.vector.svg`,
+		},
+		{
+			type: "add",
+			templateFile: "generators/service/templates/iocn.service.hbs",
+			path: "{{ path }}/services/icon.service.ts",
+		},
+		/*** icon service (end) ***/
 	],
 };
 
